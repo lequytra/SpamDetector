e5f4e033a41b189086c9adb855b848d5eaad6135
lequytra <lequytra@grinnell.edu>
2019-10-07 12:27:59 -0500

    Use Naive Bayes to train + initial work on HARN

---
+++ src/eval_utils.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def evaluate(pred, truth):
+	total = pred.shape[0]
+	res1 = pred + truth
+	res2 = pred - truth
+	assert res.shape == pred.shape
+	true_pos = res1[res1 == 2].shape[0]
+	true_neg = res1[res1 == 0].shape[0]
+	false_pos = res2[res2 == 1].shape[0]
+	false_neg = res2[res2 == -1].shape[0]
+
+	precision = true_pos/(true_pos + false_pos)
+	recall = true_pos/(true_pos + false_neg)
+	acc = (true_pos + true_neg)/total
+	f1 = 2*(precision*recall)/(precision + recall)
+	print("Precision: {}\t\t Recall: {}\t\t F1 Score: {}\t\t Accuracy: {}".format(precision, recall, f1, acc))
